5d180d1e2c4cad8de6747ee957902b3d088614d9
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-11 01:47:39 +0300

    renders BotCollection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import BotCollection from './components/BotCollection';
 
 function App() {
+  // BotCollection component is added here
   return (
     <div className="App">
      <Header />
-     
+     <BotCollection />
     </div>
   );
 }
