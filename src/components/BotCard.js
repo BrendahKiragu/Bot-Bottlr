9f74430e23383ac65f8168099e8e01bd4807f995
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-11 01:51:21 +0300

    adds props describing each bot from botCollection component

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,8 +1,28 @@
 import React from "react";
 
-function BotCard (props) {
+function BotCard ({  key,
+  name,
+  health,
+  damage,
+  armor,
+  bot_class,
+  catchphrase,
+  avatar_url,
+  created_at,
+  updated_at}){
   return(
-    <div className="bot-card"> </div>
+  <div id="bot-card">
+      <img src={avatar_url} alt={name} />
+      
+      <h2>{name}</h2>
+      <p>Health: {health}</p>
+      <p>Damage: {damage}</p>
+      <p>Armor: {armor}</p>
+      <p>Class: {bot_class}</p>
+      <p>Catchphrase: {catchphrase}</p>
+      <p>Created At: {new Date(created_at).toLocaleDateString()}</p>
+      <p>Updated At: {new Date(updated_at).toLocaleDateString()}</p>
+  </div>
   )
 }
 
