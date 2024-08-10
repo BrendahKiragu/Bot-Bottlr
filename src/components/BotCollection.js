ce841a87fe3b3dc8ef54febf5f831d3226aea35f
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-11 01:56:39 +0300

    renders each bot in the bot collection

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -2,11 +2,53 @@ import React, {useState, useEffect} from 'react';
 import BotCard from './BotCard';
 
 function BotCollection() {
-  return(
-    <div id="bot-collection">
-      <h2>Bots collection</h2>
-      <BotCard />
-    </div>
-  )
+  const [bots, setBots] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/bots')
+     .then((response) => response.json())
+     .then((data) => setBots(data));
+    //  .catch((error) => console.error('Error:', error));
+  }, []);
+
+  // Bots are added here as list items in the ul tag
+  // fetched bots from db.json
+  // map through fetched bots and render as list items
+const botObj = bots.map((bot)=>({
+  key: bot.id,
+  name: bot.name,
+  health: bot.health,
+  damage: bot.damage,
+  armor: bot.armor,
+  bot_class: bot.bot_class,
+  catchphrase: bot.catchphrase,
+  avatar_url: bot.avatar_url,
+  created_at: bot.created_at,
+  updated_at: bot.updated_at
+}))
+// console.log(botObj)
+
+return(
+  <div id="bot-collection">
+    <h2>Bot Collection</h2>
+    <ul>
+      {botObj.map((bot) => (
+        <BotCard
+          key={bot.key}
+          name={bot.name}
+          health={bot.health}
+          damage={bot.damage}
+          armor={bot.armor}
+          bot_class={bot.bot_class}
+          catchphrase={bot.catchphrase}
+          avatar_url={bot.avatar_url}
+          created_at={bot.created_at}
+          updated_at={bot.updated_at}
+        />
+      ))}
+    </ul>
+  </div>
+ );
 }
+
 export default BotCollection;
