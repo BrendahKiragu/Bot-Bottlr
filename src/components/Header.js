16b58aa4354c0ba09f549ed2be05e94689ecfafc
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-11 00:30:10 +0300

    renders Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header (){
+  return(
+  <div>
+    <h1>Bot Battler</h1>
+    <p>Welcome to the Battle of the Bots</p>
+  </div>
+  )
+}
+
+export default Header;
