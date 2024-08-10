e8841d4ddce1d03060ffe07fdd6cd6c2be8cd20c
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-11 00:08:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
+       <h1>Bot Bottlr</h1>
+       <p>Welcome to **Bot Battlr** <br></br> The one and only spot in the known universe where you can custom build your own Bot Army!</p>
       </header>
     </div>
   );
 }
 
 export default App;
+//header
+//
