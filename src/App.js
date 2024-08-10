1cea3b6a3d18394dffcd0a379b95c8db43b2c19e
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-11 00:31:29 +0300

    renders Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>Bot Bottlr</h1>
-       <p>Welcome to **Bot Battlr** <br></br> The one and only spot in the known universe where you can custom build your own Bot Army!</p>
-      </header>
+     <Header />
+     
     </div>
   );
 }
