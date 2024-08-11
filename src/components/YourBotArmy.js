f10e0684d311fb0fa5772ced2efdf3321f4f64a3
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-11 23:26:17 +0300

    first commit, creates the component

---
+++ src/components/YourBotArmy.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function YourBotArmy () {
+  return(
+  <div id="your-bot-army">
+    <h2>Check Out My Bot Army</h2>
+    <p> test</p>
+    {/* Add your bot cards here */}
+  </div>
+  )
+}
+export default YourBotArmy;
