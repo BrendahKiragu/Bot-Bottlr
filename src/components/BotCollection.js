d632eeb7b31c60f3f1b4d6eaa54346d2764b21ce
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-11 23:27:01 +0300

    function to deletea bot from collection

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import BotCard from './BotCard';
+import YourBotArmy from './YourBotArmy';
 
 function BotCollection() {
   const [bots, setBots] = useState([]);
@@ -8,13 +9,32 @@ function BotCollection() {
     fetch('http://localhost:4000/bots')
      .then((response) => response.json())
      .then((data) => setBots(data));
-    //  .catch((error) => console.error('Error:', error));
   }, []);
 
-  // Bots are added here as list items in the ul tag
-  // fetched bots from db.json
-  // map through fetched bots and render as list items
+// deletes a bot from UI and backend, with a delete confirmation message
+const eraseBot = (id) => {
+  const isConfirmed = window.confirm(`Are you sure you want to delete bot ${id}?`);
+  if (isConfirmed) {
+    fetch(`http://localhost:4000/bots/${id}`, {
+      method: 'DELETE',
+    })
+
+    .then(res => {
+      if (!res.ok) {
+        throw new Error("Error deleting bot");
+      }
+      setBots(bots.filter((bot)=> bot.id !== id))
+    })
+
+    .catch(error => {
+      console.error("Delete request failed, please try again later:", error);
+    });
+  }
+};
+
+//Details to be dispalyed from each bot on the BotCard component
 const botObj = bots.map((bot)=>({
+  id: bot.id,
   key: bot.id,
   name: bot.name,
   health: bot.health,
@@ -26,28 +46,34 @@ const botObj = bots.map((bot)=>({
   created_at: bot.created_at,
   updated_at: bot.updated_at
 }))
-// console.log(botObj)
+
 
 return(
-  <div id="bot-collection">
+  <div className="bot-collection" >
     <h2>Bot Collection</h2>
-    <ul>
+    <ul id='bot-list'>
       {botObj.map((bot) => (
         <BotCard
-          key={bot.key}
+          key={bot.id}
+          id={bot.id}
           name={bot.name}
           health={bot.health}
           damage={bot.damage}
           armor={bot.armor}
-          bot_class={bot.bot_class}
+           bot_class={bot.bot_class}
           catchphrase={bot.catchphrase}
           avatar_url={bot.avatar_url}
           created_at={bot.created_at}
           updated_at={bot.updated_at}
+          onDelete={eraseBot}
         />
       ))}
     </ul>
+    <div className="your-bot-army">
+      <YourBotArmy />
+    </div>
   </div>
+
  );
 }
 
