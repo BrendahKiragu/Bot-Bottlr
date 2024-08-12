0ec2cf1313a988df78f6d57334fb93b00db1c000
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-12 13:35:14 +0300

    creates a toggle state to hid bot details

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,25 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function YourBotArmy ({bots}) {
+ const [showDetails, setShowDetails] = useState(false)
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
   return(
-  <div id ="your-bot-army">
+  <div id ="your-bot-army" onClick={toggleDetails}>
     <h2>Create Your Bot Army</h2>
-    <ul>
-     {bots.map(bot => (
-      <li id ="your-bot-army" key={bot.id}>
-         <img src={bot.avatar_url} alt={bot.name} />
-         <p> id={bot.id}</p>
-         <p> Name={bot.name}</p>
-         <p> Health={bot.health}</p>
-         <p> Damage={bot.damage}</p>
-         <p> Armor={bot.armor}</p>
-         <p> Class={bot.bot_class}</p>
-         <p> Catchphrase={bot.catchphrase}</p>
-         <p>Created At={bot.created_at}</p>
-         <p> Updated At={bot.updated_at}</p>
-      </li>
-     ))}
-    </ul>
+    {bots.map(bot => (
+         <div key={bot.id}>
+          <img src={bot.avatar_url} alt={bot.name} />
+          {showDetails && (
+             <ul>
+               <li>
+                  <p> id={bot.id}</p>
+                  <p> Name={bot.name}</p>
+                  <p> Health={bot.health}</p>
+                  <p> Damage={bot.damage}</p>
+                  <p> Armor={bot.armor}</p>
+                  <p> Class={bot.bot_class}</p>
+                  <p> Catchphrase={bot.catchphrase}</p>
+                  <p>Created At={bot.created_at}</p>
+                  <p> Updated At={bot.updated_at}</p>
+               </li>
+              </ul> 
+          )}
+         </div>
+       ))}
   </div>
   )
 }
