3717e6cf35bf3f45b51166acfe24e6b764479aab
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-12 13:14:36 +0300

    updates yourbotarmy state when a bot is added

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,11 +1,25 @@
 import React from "react";
 
-function YourBotArmy () {
+function YourBotArmy ({bots}) {
   return(
-  <div id="your-bot-army">
-    <h2>Check Out My Bot Army</h2>
-    <p> test</p>
-    {/* Add your bot cards here */}
+  <div id ="your-bot-army">
+    <h2>Create Your Bot Army</h2>
+    <ul>
+     {bots.map(bot => (
+      <li id ="your-bot-army" key={bot.id}>
+         <img src={bot.avatar_url} alt={bot.name} />
+         <p> id={bot.id}</p>
+         <p> Name={bot.name}</p>
+         <p> Health={bot.health}</p>
+         <p> Damage={bot.damage}</p>
+         <p> Armor={bot.armor}</p>
+         <p> Class={bot.bot_class}</p>
+         <p> Catchphrase={bot.catchphrase}</p>
+         <p>Created At={bot.created_at}</p>
+         <p> Updated At={bot.updated_at}</p>
+      </li>
+     ))}
+    </ul>
   </div>
   )
 }
